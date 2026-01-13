3357ad3b54d1601bbfdd51703e035faac8f51bc9
Mike <vmichele80@hotmail.com>
2026-01-14 00:34:36 +0100

    bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -5,17 +5,35 @@ def load_data(file_path):
   with open(file_path, "r") as handle:
     return json.load(handle)
 
+def load_html():
+    fname = 'animals_template.html' #this html file is stored on the same folder of the code file
+    html_file = open(fname, 'r')
+    source_code = html_file.read()
+    return source_code
 
+def animals_cards(animals_data):
+    output = ''
+    for animal in animals_data:
+        characteristics = animal["characteristics"]
+        output += "<li>"
+        output += f"Name: {animal["name"]}\n"
+        output += f"Diet: {characteristics["diet"]}\n"
+        output += f"Location: {animal["locations"][0]}\n"
+        if "type" in characteristics:
+            output += f"Type: {characteristics["type"]}\n"
+        output += "</li>"
+    return output
 
 def main():
     animals_data = load_data('animals_data.json')
+    html_code = load_html()
 
-    for animal in animals_data:
-        characteristics = animal["characteristics"]
-        if "type" in characteristics:
-            print(f"Name: {animal["name"]} \nDiet: {characteristics["diet"]}\nLocation: {animal["locations"][0]}\nType: {characteristics["type"]}\n")
-        else:
-            print(f"Name: {animal["name"]} \nDiet: {characteristics["diet"]}\nLocation: {animal["locations"][0]}\n")
+    animals_list = animals_cards(animals_data)
+
+    new_html_code = html_code.replace("__REPLACE_ANIMALS_INFO__", animals_list)
+    # print(new_html_code)
+    with open("animals.html", "a") as f:
+        f.write(new_html_code)
 
 if __name__ == '__main__':
     main()
