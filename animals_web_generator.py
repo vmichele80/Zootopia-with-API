01edc0a0cc37e0ab4d9bd6afbc57c8582b273f07
Mike <vmichele80@hotmail.com>
2026-01-14 01:11:35 +0100

    improve the PEP 8 conformity and some changes to the loading of the html file

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,40 +1,40 @@
 import json
 
 def load_data(file_path):
-  """ Loads a JSON file """
-  with open(file_path, "r") as handle:
-    return json.load(handle)
+    """it loads a JSON file"""
+    with open(file_path, "r", encoding="utf-8") as handle:
+        return json.load(handle)
 
 def load_html():
-    fname = 'animals_template.html' #this html file is stored on the same folder of the code file
-    html_file = open(fname, 'r')
-    source_code = html_file.read()
+    """it loads a the HTML code from the template"""
+    with open("animals_template.html", "r", encoding="utf-8") as fname:
+        source_code = fname.read()
     return source_code
 
+
 def animals_cards(animals_data):
-    output = '' # define an empty string
+    """it generates all the animal cards"""
+    output = ''  # define an empty string
     for animal in animals_data:
         # append information to each string
         characteristics = animal["characteristics"]
         output += '<li class="cards__item">'
         output += f'<div class="card__title"> {animal["name"]}</div>\n'
-        output += '<p class ="card__text" >'
-        output += f'<strong>Diet: </strong>{characteristics['diet']}<br/>\n'
-        output += f'<strong>Location: </strong>{animal["locations"][0]}<br/>\n'
+        output += '<div class ="card__text" >\n<ul>'
+        output += f'<li><strong>Diet: </strong>{characteristics["diet"]}</li>\n'
+        output += f'<li><strong>Location: </strong>{animal["locations"][0]}</li>\n'
         if "type" in characteristics:
-            output += f'<strong>Type: </strong>{characteristics["type"]}<br/>\n'
-        output += '</p>\n</li>'
+            output += f'<li><strong>Type: </strong>{characteristics["type"]}</li>\n'
+        output += '</ul>\n</div>\n</li>'
     return output
 
 def main():
     animals_data = load_data('animals_data.json')
     html_code = load_html()
-
     animals_list = animals_cards(animals_data)
-
     new_html_code = html_code.replace("__REPLACE_ANIMALS_INFO__", animals_list)
-    # print(new_html_code)
-    with open("animals.html", "a") as f:
+
+    with open("animals.html", "w", encoding="utf-8") as f:  # Write the new html code to a file
         f.write(new_html_code)
 
 if __name__ == '__main__':
